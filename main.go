8725ee8e0d091b82f3fb36b35f0b23196dfa42a4
Matt Gaede <mgaede01@gmail.com>
2021-11-11 19:29:16 -0600

    Created ProduceItem structure

--- main.go
+++ main.go
@@ -4,6 +4,16 @@ import (
 	"fmt"
 )
 
+type ProduceItem struct {
+	ProduceCode string  `json:"ProduceCode"`
+	Name        string  `json:"Name"`
+	UnitPrice   float64 `json:"UnitPrice"`
+}
+
+var produceList = []ProduceItem{
+	{ProduceCode: "ABCD-EFGH-IJKL-MNOP", Name: "Zucchini", UnitPrice: 1.17},
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(produceList)
 }
