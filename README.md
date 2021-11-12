17e7f687014a7bd218458b1e815d4e4cbaced41f
Matt Gaede <mgaede01@gmail.com>
2021-11-11 22:25:52 -0600

    Added DELETE request

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ A take-home assessment using a Golang backend to create a supermarket API
 ## Installation
 Requires installation of gin:
 Look at https://github.com/gin-gonic/gin for installation details
+
+## Assumptions
+ - Order of the list does not matter
+ - List items have unique identifiers, therefore the list should not contain duplicates
+ 
--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 )
 
 type ProduceItem struct {
-	ProduceCode string  `json:"ProduceCode"`
+	ProduceCode string  `json:"Produce Code"`
 	Name        string  `json:"Name"`
-	UnitPrice   float64 `json:"UnitPrice"`
+	UnitPrice   float64 `json:"Unit Price"`
 }
 
 var produceList = []ProduceItem{
@@ -30,9 +30,42 @@ func postProduceItem(c *gin.Context) {
 	if err := c.BindJSON(&newItem); err != nil {
 		return
 	}
+	// Check if item already exists
+	for i, item := range produceList {
+		_ = i
+		if newItem == item {
+			c.String(http.StatusBadRequest, "Item already exists\n")
+			return
+		}
+	}
 
+	// Add new item
 	produceList = append(produceList, newItem)
 	c.IndentedJSON(http.StatusCreated, newItem)
+}
+
+func removeFromList(i int) {
+	produceList[i] = produceList[len(produceList)-1]
+	produceList = produceList[:len(produceList)-1]
+}
+
+func deleteProduceItem(c *gin.Context) {
+	var produceCode string
+	index := -1
+
+	produceCode = c.Param("produceCode")
+	// Search for Produce Code in list
+	for i, item := range produceList {
+		if item.ProduceCode == produceCode {
+			index = i
+		}
+	}
+	if index == -1 {
+		c.String(http.StatusNoContent, "Item with Produce Code %s not found.\n", produceCode)
+		return
+	}
+	removeFromList(index)
+	c.String(http.StatusOK, "Item with Produce Code %s deleted.\n", produceCode)
 }
 
 func main() {
@@ -40,5 +73,6 @@ func main() {
 	router := gin.Default()
 	router.GET("/", getProduceList)
 	router.POST("/", postProduceItem)
+	router.DELETE("/:produceCode", deleteProduceItem)
 	router.Run("localhost:8080")
 }
