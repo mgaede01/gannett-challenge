12e4a2679aeb73f238438a4fa72cd4f56aa913d9
Matt Gaede <mgaede01@gmail.com>
2021-11-11 19:47:13 -0600

    Integrated gin, Successful GET request

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Gannett Challenge
 A take-home assessment using a Golang backend to create a supermarket API
+
+## Installation
+Requires installation of gin:
+Look at https://github.com/gin-gonic/gin for installation details
--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 type ProduceItem struct {
@@ -14,6 +17,13 @@ var produceList = []ProduceItem{
 	{ProduceCode: "ABCD-EFGH-IJKL-MNOP", Name: "Zucchini", UnitPrice: 1.17},
 }
 
+func getProduceList(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, produceList)
+}
+
 func main() {
-	fmt.Println(produceList)
+	fmt.Println("Starting gin...")
+	router := gin.Default()
+	router.GET("/", getProduceList)
+	router.Run("localhost:8080")
 }
